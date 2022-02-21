f4d524aa373ffe5181fed5ffca73141ad9ae03e8
Rachelle Hanna <ha.rachelle@gmail.com>
2022-02-21 17:27:39 -0400

    Update page three and four with JS

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-//add event listener on 'playButton'
+const appState = {
+    dustIndex: null,
+    smileIndex: null,
+};
 
 const playButtonClick = document.getElementById("playButton");
 
@@ -6,13 +9,56 @@ const pageOne = document.getElementById("pageOne");
 
 const pageTwo = document.getElementById("pageTwo");
 
+const pageThree = document.getElementById("pageThree");
+
+const pageFour = document.getElementById("pageFour");
+
 playButtonClick.addEventListener("click", function () {
     pageOne.classList.add("fadeOut");
-    playButtonClick.classList.add("fadeOut");
 });
 
 pageOne.addEventListener("transitionend", function () {
     pageOne.classList.add("hidden");
     pageTwo.classList.remove("hidden");
     pageTwo.classList.add("fadeIn");
+});
+
+//Page two
+
+const dustForms = document.querySelectorAll(".dustForm");
+
+dustForms.forEach(function (dustForm, index) {
+    dustForm.addEventListener("click", function () {
+        appState.dustIndex = index;
+        pageTwo.classList.add("fadeOut");
+    });
+});
+
+pageTwo.addEventListener("transitionend", function () {
+    pageTwo.classList.add("hidden");
+    pageThree.classList.remove("hidden");
+    pageThree.classList.add("fadeIn");
+});
+
+//Page three
+
+const smileForms = document.querySelectorAll(".smiles");
+
+smileForms.forEach(function (smiles, index) {
+    smiles.addEventListener("click", function () {
+        appState.smileIndex = index;
+        pageThree.classList.add("fadeOut");
+    });
+});
+
+pageThree.addEventListener("transitionend", function () {
+    pageThree.classList.add("hidden");
+
+    // Before we reveal page 4 we insert the correct images into it.
+
+    const dustBody = document.createElement("img");
+    dustBody.setAttribute("src", `assets/body${appState.dustIndex + 1}.png`);
+    pageFour.appendChild(dustBody);
+    pageFour.classList.remove("hidden");
+    pageFour.classList.add("fadein");
 });
